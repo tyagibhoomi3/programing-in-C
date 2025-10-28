d6103464d34144c3021962c5ea8f81569efb7706
tyagibhoomi3 <tyagibhoomi3@gmail.com>
2025-10-28 17:35:32 +0530

    Add files via upload

---
+++ exp5.1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int a =19;
+
+int main()
+{
+	printf("Name - Bhoomi Tyagi\n SAP ID - 590028798\n Course - BCA\n Batch - 06\n");
+    printf("--------------------------------------------------\n");
+    printf("func 0 - %d\n" ,a);
+    func1();
+    func2();
+    return 0;
+}
+
+void func1()
+{
+    a=a+1;
+    printf("func 1 - %d\n" ,a);
+
+}
+void func2()
+{
+    a =a+2;
+    printf("func 2 - %d\n" ,a);
+}
