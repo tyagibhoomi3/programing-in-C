c9dfcf5e5a5681617c8fe4f91096d80b7ba9f348
tyagibhoomi3 <tyagibhoomi3@gmail.com>
2025-10-28 17:37:19 +0530

    Add files via upload

---
+++ exp5.2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int a =19;
+
+int main()
+{
+	printf("Name - Bhoomi Tyagi\n SAP ID - 590028798\n Course - BCA\n Batch - 06\n");
+    printf("--------------------------------------------------\n");
+    printf("func 0 - %d\n" ,a);
+    func1();
+    func2();
+    return 0;
+}
+
+void func1()
+{
+    int b =1;
+    a=a+1;
+    printf("func 1 - %d\n" ,a);
+
+}
+void func2()
+{
+    a =a+2;
+    printf("func 2 - %d\n" ,a);
+    printf("Tring to print func 1 local variable here %d" ,b); // but it gives a error as we cannot use local variable in func to another
+}
